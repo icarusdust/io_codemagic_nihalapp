46b32a6d2c82fda45e7c216718a120236f519936
Nihal Agazade <nihalagazade@162-33-191-90.dyn.estpak.ee>
2021-06-01 12:39:50 +0300

    Firebase core features

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import "package:flutter/material.dart";
 import "./text.dart";
+import "package:firebase_core/firebase_core.dart";
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget{
   _MyAppState createState() => _MyAppState();
